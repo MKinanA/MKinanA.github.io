30fdacd29cb3d0f4f039290787fb20f6d3698ad1
MKinanA <mkinanahsan@gmail.com>
2023-09-26 10:10:02 +0700

    it's now responsive

--- js/script.js
+++ js/script.js
@@ -1,48 +1,52 @@
-// // Active Section Detection
-// let sections = document.querySelectorAll('section');
-// let navlinks = document.querySelectorAll('header nav a');
-// window.onscroll = () => {
-//     sections.forEach(sec => {
-//         let top = window.scrollY;
-//         let offset = sec.offsetTop - 150;
-//         let height = sec.offsetHeight;
-//         let id = sec.getAttribute('id');
-//         console.log(id + ' ( ' + top + ' >= ' + offset + ' && ' + top + ' < ' + offset + ' + ' + height + ' )')
-//         if(top >= offset && top < offset + height) {
-//             navlinks.forEach(links => {
-//                 // console.log(links)
-//                 links.classList.remove('active');
-//                 // console.log(document.querySelector('header nav a[href*=' + id + ']'))
-//                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-//             });
-//         };
-//     });
-// };
+// Toggle Menu
+let menuIcon = document.querySelector('#menu-icon');
+let nav = document.querySelector('.nav');
+let sections = document.querySelector('section');
+let body = document.querySelector('.body');
 
-document.addEventListener('DOMContentLoaded', function() {
-    const sections = document.querySelectorAll("section");
-    const navLinks = document.querySelectorAll("header nav a");
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    nav.classList.toggle('active');
+}
 
-    const observerOptions = {
-        root: null,
-        rootMargin: "0px",
-        threshold: 0.5,
-    };
-
-    const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-            const id = entry.target.getAttribute("id");
-            const navLink = document.querySelector(`header nav a[href="#${id}"]`);
-
-            if (entry.isIntersecting) {
-                navLinks.forEach(link => link.classList.remove("active"));
-                navLink.classList.add("active");
-            }
-        });
-    }, observerOptions);
-
-    sections.forEach(section => {
-        observer.observe(section);
-    });
+nav.addEventListener("click", function(){
+    menuIcon.classList.remove('bx-x');
+    nav.classList.remove("active");
 });
-// Thank you ChatGPT GPT-3.5 :))))
+
+sections.addEventListener("click", function(){
+    menuIcon.classList.remove('bx-x');
+    nav.classList.remove("active");
+});
+
+window.onscroll = ()=>{
+    console.log('scrolled')
+    menuIcon.classList.remove('bx-x');
+    nav.classList.remove("active");
+};
+
+body.onscroll = ()=>{
+    console.log('scrolled')
+    menuIcon.classList.remove('bx-x');
+    nav.classList.remove("active");
+};
+
+// Active Section Detection
+let navlinks = document.querySelectorAll('header nav a');
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        console.log(id + ' ( ' + top + ' >= ' + offset + ' && ' + top + ' < ' + offset + ' + ' + height + ' )')
+        if(top >= offset && top < offset + height) {
+            navlinks.forEach(links => {
+                // console.log(links)
+                links.classList.remove('active');
+                // console.log(document.querySelector('header nav a[href*=' + id + ']'))
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+};
